7a8280137eb1240c0a38ca32c21208c89ff07417
Xeyal Ism <Asus@LAPTOP-PJ5F6UB5>
2024-02-07 18:08:29 +0400

    Migration updates

---
+++ NetflixAppDataAccessLayer/Migrations/20240207140159_Mig3.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetflixAppDataAccessLayer.Migrations
+{
+    public partial class Mig3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
